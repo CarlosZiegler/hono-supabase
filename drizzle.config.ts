7c72b4fd3fe2de7c2ca8a6061f518b2332ed175a
carlosziegler <carlos.ziegler@gmail.com>
2024-05-13 12:50:01 +0200

    chore: update deps

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
 import { env } from "@/config/env";
 import type { Config } from "drizzle-kit";
 
-export default ({
-	schema: "./src/libs/database/schema.ts",
-	out: "./drizzle",
-	driver: "pg",
-	dbCredentials: {
-		connectionString: env.DATABASE_URL,
-	},
-} satisfies Config);
+export default {
+  schema: "./src/libs/database/schema.ts",
+  out: "./drizzle",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+} satisfies Config;
